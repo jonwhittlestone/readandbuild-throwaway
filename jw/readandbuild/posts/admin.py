68a1fd1e7395e1a3f8e18d707ab4e236c8cc4e6c
Jon Whittlestone <jon@howapped.com>
2016-09-28 18:10:44 +0100

    simple post_home

--- jw/readandbuild/posts/admin.py
+++ jw/readandbuild/posts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+# Register your models here.
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp", "updated"]
+    list_display_links = ["title"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
