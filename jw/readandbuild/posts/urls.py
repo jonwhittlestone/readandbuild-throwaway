86b6ff33b67b834aae07af7203f9849323fb660e
Jon Whittlestone <jon@howapped.com>
2016-09-28 18:20:00 +0100

    simple CRUD for views

--- jw/readandbuild/posts/urls.py
+++ jw/readandbuild/posts/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
-    url(r'^$', "posts.views.post_home")
+    url(r'^create/$', "posts.views.post_create"),
+    url(r'^detail/$', "posts.views.post_detail"),
+    url(r'^$', "posts.views.post_list"),
+    url(r'^update/$', "posts.views.post_update"),
+    url(r'^delete/$', "posts.views.post_delete")
+
 ]
--- jw/readandbuild/posts/views.py
+++ jw/readandbuild/posts/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_home(request):
-    return HttpResponse("<h1>Welcome</h1><p>readandbuild.howapped.com</p>");
+def post_create(request):
+    return HttpResponse("<h1>Welcome</h1><p>readandbuild.howapped.com</p><p>Create</p>");
+
+def post_detail(request): # read
+    return HttpResponse("<h1>Welcome</h1><p>readandbuild.howapped.com</p><p>Delete</p>");
+
+def post_list(request):
+    return HttpResponse("<h1>Welcome</h1><p>readandbuild.howapped.com</p><p>List</p>");
+
+def post_update(request):
+    return HttpResponse("<h1>Welcome</h1><p>readandbuild.howapped.com</p><p>Update</p>");
+
+def post_delete(request):
+    return HttpResponse("<h1>Welcome</h1><p>readandbuild.howapped.com</p><p>Delete</p>");
